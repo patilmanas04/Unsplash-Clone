7f54613d6148b19cdd13c33112b7f75c86b24600
manas <pmanas13092004@gmail.com>
2023-04-14 01:43:30 +0530

    Added new features

--- js/image_api.js
+++ js/image_api.js
@@ -8,14 +8,40 @@ editorialImageAJAXCall.open('GET', url1);
 editorialImageAJAXCall.send();
 
 editorialImageAJAXCall.addEventListener("load", function (e){
-    console.log(e);
     let response = JSON.parse(e.target.response);
-    console.log(response);
     addImageToDOM(response);
 });
 
 function addImageToDOM(response){
     let imageContainer = document.querySelector(".container .editorial-title-details");
-    console.log(imageContainer);
     imageContainer.style.backgroundImage = `url(${response.urls.full})`;
+}
+
+
+
+
+
+// Adding the images to the gallery dynamically
+let url2 = "https://api.unsplash.com/photos/random?client_id=HB6DnSO4g5aZ4oHbaFwFK5IZUareL-qF5kuq8VuGwZk&count=30";
+
+let galleryImageAJAXCall = new XMLHttpRequest();
+galleryImageAJAXCall.open('GET', url2);
+galleryImageAJAXCall.send();
+
+galleryImageAJAXCall.addEventListener("load", function(e){
+    let response = JSON.parse(e.target.response);
+    appendImageToGallery(response);
+});
+
+function appendImageToGallery(response){
+    console.log(response);
+    response.forEach(function(e){
+        let imageUrl = e.urls.regular;
+        console.log(imageUrl);
+        let gallery = document.querySelector(".gallery");
+        let imageContainer = document.createElement('img');
+        imageContainer.src = imageUrl;
+        imageContainer.className = "gallery-image";
+        gallery.appendChild(imageContainer);
+    });
 }
